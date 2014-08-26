f4e344d937760bd48fd716fcc6aa38948ee263b0
Sean Marshallsay <srm.1708@gmail.com>
2014-08-26 18:46:57 +0100

    Error checking when searching for libsqlite3.
    Throw error if an empty string is returned.

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,16 @@
 const SPECIALDB = SpecialDBEnum()
-const SQLITELIB = find_library(["libsqlite3"], [get(ENV, "SEKELIA_SQLITE", [])])
-
+const SQLITELIB = begin
+    local lib = find_library(
+        ["libsqlite3"],
+        [get(ENV, "SEKELIA_SQLITE", [])]
+    )
+    if lib == ""
+        info("libsqlite3 could not be found.")
+        info("consider setting SEKELIA_SQLITE environment variable.")
+        error("SQLite3 library could not be loaded.")
+    else
+        lib
+    end
+end
 
 const SQLITE_OK = 0
