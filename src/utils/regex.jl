5b8c0645a9f7c01ba445400b7bc709b65b82b28f
Sean Marshallsay <srm.1708@gmail.com>
2014-10-01 00:40:23 +0100

    Use api module in regex.jl

--- src/utils/regex.jl
+++ src/utils/regex.jl
@@ -1,41 +1,15 @@
 function regex(context, nargs, values)
-    rgx_val = unsafe_load(values::Ptr{Ptr{Void}}, 1)
-    rgx = bytestring(
-        ccall(
-            (:sqlite3_value_text, api.SQLITELIB),
-            Ptr{Uint8},
-            (Ptr{Void},),
-            rgx_val
-        )
-    )
+    rgx_val = unsafe_load(values, 1)
+    rgx = api.sqlite3_value_text(rgx_val)
 
-    str_val = unsafe_load(values::Ptr{Ptr{Void}}, 2)
-    str = bytestring(
-        ccall(
-            (:sqlite3_value_text, api.SQLITELIB),
-            Ptr{Uint8},
-            (Ptr{Void},),
-            str_val
-        )
-    )
+    str_val = unsafe_load(values, 2)
+    str = api.sqlite3_value_text(str_val)
 
     r = Regex(rgx)
     if ismatch(r, str)
-        ccall(
-            (:sqlite3_result_int, api.SQLITELIB),
-            Void,
-            (Ptr{Void}, Cint),
-            context,
-            1
-        )
+        api.sqlite3_result_int(context, 1)
     else
-        ccall(
-            (:sqlite3_result_int, api.SQLITELIB),
-            Void,
-            (Ptr{Void}, Cint),
-            context,
-            0
-        )
+        api.sqlite3_result_int(context, 0)
     end
 
     nothing
